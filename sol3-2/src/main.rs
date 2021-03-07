03c583690adb18deb5a155c47ac99658fccee22c
pettan0818 <pettan0818+git@gmail.com>
2021-03-08 07:38:01 +0900

    Refer to sol.

--- sol3-2/src/main.rs
+++ sol3-2/src/main.rs
@@ -1,25 +1,26 @@
 fn main() {
     let x = vec![100, 10, 20, 30, 40, 30, 1];
-    println!("{}", search(x));
+    let res = 10;
+
+    println!("FINAL: {} / ANS: {}", search(x), res);
 }
 
 fn search(target_vec: Vec<i32>) -> i32 {
     // 2番目に小さな値を求める。
-    let mut least = 9_999_999;
-    let mut second_least = 9_999_998;
-    let mut flag = false;
+    let max = i32::MAX;
+    let mut least = max;
+    let mut second_least = max;
     for i in target_vec.iter() {
         if i < &least {
-            if flag {
-                second_least = least;
-            }
+            second_least = least;
             least = *i;
-            flag = true;
-            continue;
-        }
-        if i > &least && i < &second_least {
+        } else if i < &second_least {
             second_least = *i;
         }
+        println!(
+            "least: {} \n second_least: {}\n==========",
+            least, second_least
+        );
     }
     second_least
 }
