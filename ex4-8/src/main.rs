412fe06d409ba6e9e3e43e6fac23200aa1f52b12
pettan0818 <pettan0818+git@gmail.com>
2021-03-09 06:40:27 +0900

    Done Ex4-8.

--- ex4-8/src/main.rs
+++ ex4-8/src/main.rs
@@ -1,19 +1,25 @@
 fn main() {
-    let n = 6;
-    let res = fibo(n);
+    let n: i64 = 49;
+    let mut memo = vec![-1; (n + 1) as usize];
+    let res = fibo(n, &mut memo);
+
     println!("Term {}: {}", n, res);
 }
 
-fn fibo(n: i32) -> i32 {
+fn fibo(n: i64, memo: &mut Vec<i64>) -> i64 {
     // Report Call Another explorer.
-    //println!("Called fibo({})", n);
+    println!("Called fibo({})", n);
     if n == 0 {
         return 0;
     }
     if n == 1 {
         return 1;
     }
-    let res = fibo(n - 1) + fibo(n - 2);
+    if memo[n as usize] != -1 {
+        return memo[n as usize];
+    }
+    let res = fibo(n - 1, memo) + fibo(n - 2, memo);
     //println!("No.{}: {}", n, res);
+    memo[n as usize] = res;
     res
 }
