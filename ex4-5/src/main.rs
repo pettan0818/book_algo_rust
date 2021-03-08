4979058b62b5e45884e8d3098accb3d6a99a5415
pettan0818 <pettan0818+git@gmail.com>
2021-03-09 05:43:44 +0900

    Done Ex4-5,4-6

--- ex4-5/src/main.rs
+++ ex4-5/src/main.rs
@@ -1,14 +1,19 @@
 fn main() {
-    let res = gcd(15, 51);
-    assert_eq!(res, 3);
-    println!("{}", res);
+    let n = 6;
+    let res = fibo(n);
+    println!("Term {}: {}", n, res);
 }
 
-fn gcd(m: i32, n: i32) -> i32 {
+fn fibo(n: i32) -> i32 {
     // Report Call Another explorer.
-    let mod_both = m % n;
-    if mod_both == 0 {
-        return n;
+    //println!("Called fibo({})", n);
+    if n == 0 {
+        return 0;
     }
-    gcd(n, mod_both)
+    if n == 1 {
+        return 1;
+    }
+    let res = fibo(n - 1) + fibo(n - 2);
+    //println!("No.{}: {}", n, res);
+    res
 }
