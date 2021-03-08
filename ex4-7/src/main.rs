bf4b506a609c0cf5b6a90e10bc00bbb776bc3c7b
pettan0818 <pettan0818+git@gmail.com>
2021-03-09 05:54:43 +0900

    Done Ex4-7.

--- ex4-7/src/main.rs
+++ ex4-7/src/main.rs
@@ -1,19 +1,9 @@
 fn main() {
-    let n = 6;
-    let res = fibo(n);
-    println!("Term {}: {}", n, res);
-}
+    let mut result_vec: Vec<i64> = vec![0, 1];
+    let target = 50;
 
-fn fibo(n: i32) -> i32 {
-    // Report Call Another explorer.
-    //println!("Called fibo({})", n);
-    if n == 0 {
-        return 0;
+    for i in 2..target {
+        result_vec.push(result_vec[i - 1] + result_vec[i - 2]);
     }
-    if n == 1 {
-        return 1;
-    }
-    let res = fibo(n - 1) + fibo(n - 2);
-    //println!("No.{}: {}", n, res);
-    res
+    println!("{}", result_vec.last().unwrap());
 }
