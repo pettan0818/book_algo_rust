441e7393fa81af4bfe811950ef9f976f6199b2d6
pettan0818 <pettan0818+git@gmail.com>
2021-03-18 21:33:04 +0900

    Added but no good.

--- sol4-5/src/main.rs
+++ sol4-5/src/main.rs
@@ -1,30 +1,66 @@
-use std::collections::HashMap;
+fn main() {}
+// use itertools::join;
+// use permutator::Permutation;
+// use std::collections::HashSet;
 
-fn main() {
-    let n: i128 = 101;
+// fn main() {
+//     let n: f32 = 1000000 as f32;
+//     let x = n.log10() as i32 + 1;
+//     println!("calc option: {}", x);
+//     let r = make_data(x);
+//     scan_from_result(r, n as i32);
+// }
 
-    let mut memo_map: HashMap<i128, i128> = HashMap::new();
-    memo_map.insert(0, 0);
-    memo_map.insert(1, 0);
-    memo_map.insert(2, 1);
+// fn scan_from_result(t: Vec<i32>, limit: i32) {
+//     let x = t.iter().filter(|num| **num < limit);
+//     println!("{}", x.clone().collect::<Vec<_>>().len());
+//     println!("{:?}", x.clone().collect::<Vec<_>>());
+// }
 
-    tribo(n, &mut memo_map);
+// fn make_data(figure_rank: i32) -> Vec<i32> {
+//     let mut candidate = vec![vec![3, 5, 7]];
+//     let mut candidate_list = vec![];
+//     candidate_list.push(vec![3, 5, 7]);
+//     let figure_rank: i32 = figure_rank - 3;
 
-    for i in 0..n + 1 {
-        println!("{}: {}", i, memo_map[&i]);
-    }
-}
+//     for _ in 0..figure_rank {
+//         candidate = make_candidate(candidate);
+//         candidate_list.extend(candidate.clone());
+//     }
 
-fn tribo(target: i128, memo: &mut HashMap<i128, i128>) -> i128 {
-    // Init.
-    if memo.contains_key(&target) {
-        return memo[&target];
-    }
+//     // println!("before::{:?}", candidate_list);
 
-    // calc
-    let x: i128 = tribo(target - 1, memo) + tribo(target - 2, memo) + tribo(target - 3, memo);
+//     let mut final_res: HashSet<i32> = HashSet::new();
 
-    memo.insert(target, x);
+//     for comp in candidate_list {
+//         let res = make_num_from_candidate(comp);
+//         final_res.extend(res);
+//     }
+//     let mut checked: Vec<i32> = final_res.into_iter().collect();
+//     checked.sort();
+//     // println!("{:?}", checked);
 
-    x
-}
+//     checked
+// }
+
+// fn make_num_from_candidate(mut component: Vec<i32>) -> HashSet<i32> {
+//     let mut res: Vec<i32> = Vec::new();
+//     component.permutation().for_each(|p| {
+//         let temp: i32 = join(&p, "").parse().unwrap();
+//         res.push(temp);
+//     });
+//     let uniq: HashSet<i32> = res.into_iter().collect();
+//     uniq
+// }
+
+// fn make_candidate(candidate: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
+//     let mut res: Vec<Vec<i32>> = Vec::new();
+//     for can in candidate {
+//         for i in vec![3, 5, 7] {
+//             let mut temp: Vec<i32> = vec![i];
+//             temp.extend(can.iter().copied());
+//             res.push(temp);
+//         }
+//     }
+//     res
+// }
