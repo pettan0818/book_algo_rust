13833c128d93e16b93886b787f8a8b41d4d49880
pettan0818 <pettan0818+git@gmail.com>
2021-03-20 06:16:53 +0900

    Add Ex5-5.

---
+++ ex5-5/src/main.rs
@@ -0,0 +1,34 @@
+// use proconio::input;
+use std::cmp::min;
+
+fn main() {
+    // input! {
+    //     n: usize,
+    //     step_cost: [i32; n]
+    // }
+
+    let n = 6;
+    let step_cost = vec![30, 10, 60, 10, 60, 50];
+
+    let mut cost_result = vec![0];
+    cost_result.extend(vec![99999; n - 1]);
+
+    println!("{}", cost_explorer(step_cost, &mut cost_result));
+}
+
+fn cost_explorer(step_cost: Vec<i32>, cost_result: &mut Vec<i32>) -> i32 {
+    for now in 1..cost_result.len() {
+        cost_result[now] = min(
+            cost_result[now - 1] + (step_cost[now - 1] - step_cost[now]).abs(),
+            cost_result[now],
+        );
+        if now > 1 {
+            cost_result[now] = min(
+                cost_result[now - 2] + (step_cost[now - 2] - step_cost[now]).abs(),
+                cost_result[now],
+            );
+        }
+    }
+    println!("{:?}", cost_result);
+    return cost_result[cost_result.len() - 1];
+}
